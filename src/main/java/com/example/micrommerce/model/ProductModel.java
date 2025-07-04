13316693fe605ab5909c4ef9c6d191fdef326628
Oreilleu <aurelien.picardpabijan@gmail.com>
2025-07-04 10:32:00 +0200

    update name table

--- src/main/java/com/example/micrommerce/model/ProductModel.java
+++ src/main/java/com/example/micrommerce/model/ProductModel.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Table(name = "Product")
+@Table(name = "products")
 @Entity
 public class ProductModel {
     @Id
