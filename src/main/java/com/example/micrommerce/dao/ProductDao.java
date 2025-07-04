055b55006d3e301e8ad8e5f5071637992fe8d071
Oreilleu <aurelien.picardpabijan@gmail.com>
2025-07-04 10:42:40 +0200

    add crud

--- src/main/java/com/example/micrommerce/dao/ProductDao.java
+++ src/main/java/com/example/micrommerce/dao/ProductDao.java
@@ -17,36 +17,19 @@ public class ProductDao {
         return productRepository.findAll();
     }
 
-    /*
-     * 
-     * public ProductModel findById(int id) {
-     * return products.stream()
-     * .filter(product -> product.getId() == id)
-     * .findFirst()
-     * .orElse(null);
-     * }
-     * 
-     * @Override
-     * public ProductModel save(ProductModel product) {
-     * if (product.getId() == 0) {
-     * // Assign a new ID to the product
-     * product.setId(products.size() + 1);
-     * products.add(product);
-     * } else {
-     * // Update existing product
-     * for (int i = 0; i < products.size(); i++) {
-     * if (products.get(i).getId() == product.getId()) {
-     * products.set(i, product);
-     * break;
-     * }
-     * }
-     * }
-     * return product;
-     * }
-     * 
-     * @Override
-     * public void deleteById(int id) {
-     * products.removeIf(product -> product.getId() == id);
-     * }
-     */
+    public ProductModel findById(int id) {
+        return productRepository.findById(id);
+    }
+
+    public ProductModel save(ProductModel product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteById(int id) {
+        productRepository.deleteById(id);
+    }
+
+    public boolean existsById(int id) {
+        return productRepository.existsById(id);
+    }
 }
